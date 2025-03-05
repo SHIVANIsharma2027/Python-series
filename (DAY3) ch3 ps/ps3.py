dd33171bcb43261dd94ef810a3bfd5091eaacff6
SHIVANIsharma2027 <shivani.nikky777@gmail.com>
2025-03-05 19:52:46 +0530

    Add files via upload

---
+++ (DAY3) ch3 ps/ps3.py
@@ -0,0 +1,6 @@
+"""
+Write a program to detect double space in a string.
+"""
+
+classs = "this is a 8th  standard."
+print(classs.find("  "))
---
+++ (DAY3) ch3 ps/ps4.py
@@ -0,0 +1,6 @@
+"""
+Replace the double space from problem 3 with single spaces.
+"""
+
+classs = "this is a 8th  standard."
+print(classs.replace("  "," "))
