f832d984434bc5067f1946b8c416432e23d6730f
SHIVANIsharma2027 <shivani.nikky777@gmail.com>
2025-03-17 18:47:09 +0530

    Delete DAY7 (ch7)/(DAY8) ch8 ps directory

--- DAY7 (ch7)/(DAY8) ch8 ps/ps6.py
+++
@@ -1,3 +0,0 @@
-def i_to_c(n):
-    print(n*2.54)
-i_to_c(5)
--- DAY7 (ch7)/(DAY8) ch8 ps/ps8.py
+++
@@ -1,6 +0,0 @@
-def table(n):
-    for i in range(1, 11):
-        print(n, 'X' ,i, "=", n*i )
-        # print(f"{n} X {i} = {n*i}")
- 
-table(3)        
