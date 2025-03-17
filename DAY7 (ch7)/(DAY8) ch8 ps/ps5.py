93f3ef3dfaec6efba472982b26d3f59b5e4f9d26
SHIVANIsharma2027 <shivani.nikky777@gmail.com>
2025-03-17 18:46:20 +0530

    Add files via upload

---
+++ DAY7 (ch7)/(DAY8) ch8 ps/ps5.py
@@ -0,0 +1,12 @@
+'''
+***
+** - for n = 3
+*
+'''
+def pattern(n):
+    if(n==0):
+        return
+    print("*"*n)
+    pattern(n-1)
+    
+pattern(5)
---
+++ DAY7 (ch7)/(DAY8) ch8 ps/ps6.py
@@ -0,0 +1,3 @@
+def i_to_c(n):
+    print(n*2.54)
+i_to_c(5)
